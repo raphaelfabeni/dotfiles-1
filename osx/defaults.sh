42dd88a40de50d15ee1b417c9b7eaf1998cce9f2
Igor APA <apa.igor@gmail.com>
2015-10-30 14:37:38 -0200

    feat(osx): Setting osx launch smooth

--- osx/defaults.sh
+++ osx/defaults.sh
@@ -241,7 +241,7 @@ defaults write com.apple.dock mineffect -string "genie"
 defaults write com.apple.dock tilesize -int 32
 
 # Don’t animate opening applications from the Dock
-defaults write com.apple.dock launchanim -bool false
+defaults write com.apple.dock launchanim -bool true
 
 # Automatically hide and show the Dock
 defaults write com.apple.dock autohide -bool true
